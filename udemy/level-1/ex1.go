c8071f3092228fcb6b34e08c480e88512953be5a
Dan Austin <djadevelopment@gmail.com>
2020-08-26 12:36:01 +0100

    Short variable declarations

---
+++ udemy/level-1/ex1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Printf("%d\t%s\t%t\n", x, y, z)
+	fmt.Println("Number:", x)
+	fmt.Println("Name:", y)
+	fmt.Println("Truth:", z)
+
+}
